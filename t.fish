631097b6582d8c5890844cbd1799e348e03e168c
LurkyDismal <lurkydismal@duck.com>
2026-06-27 06:50:58 +0000

    8

--- t.fish
+++ t.fish
@@ -1,14 +1,29 @@
 #!/usr/bin/env fish
 
-set slndir (realpath src)
+if test (count $argv) -ne 2
+    echo "Usage: "(status basename)" <path> <variable>"
+    exit 1
+end
+
+set target (realpath $argv[1])
+set variable $argv[2]
 
 fd -e vcxproj -e props -e targets | while read -l file
     set dir (dirname (realpath $file))
-    set rel (realpath --relative-to "$dir" "$slndir")
+    set rel (realpath --relative-to "$dir" "$target")
+
+    # MSBuild uses backslashes.
     set rel (string replace -a / \\ "$rel")
 
-    set rel (string replace -a / \\ "$rel")
+    # If the target is a directory, preserve the trailing slash.
+    if test -d "$target"
+        set rel "$rel\\"
+    end
+
+    # Escape backslashes for sed.
     set rel_escaped (string replace -a \\ \\\\ "$rel")
 
-    sed -i "s|\$(SolutionDir)|$rel_escaped\\\\|g" "$file"
+    set pattern '$('"$variable"')'
+
+    sed -i "s|$pattern|$rel_escaped|g" "$file"
 end
