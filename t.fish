af1d5f813675ad9e025a654882e6e55164991475
LurkyDismal <lurkydismal@duck.com>
2026-06-27 06:34:54 +0000

    7

--- t.fish
+++ t.fish
@@ -1,14 +1,20 @@
 #!/usr/bin/env fish
 
-set slndir (realpath src)
+set -l files (fd -e vcxproj -e props -e targets)
 
-fd -e vcxproj -e props -e targets | while read -l file
-    set dir (dirname (realpath $file))
-    set rel (realpath --relative-to "$dir" "$slndir")
-    set rel (string replace -a / \\ "$rel")
-
-    set rel (string replace -a / \\ "$rel")
-    set rel_escaped (string replace -a \\ \\\\ "$rel")
-
-    sed -i "s|\$(SolutionDir)|$rel_escaped\\\\|g" "$file"
+for file in $files
+    sed -i \
+        -e 's|\$(xrBinRootDir)|./../_build/bin/|g' \
+        -e 's|\$(xrBinPlugRootDir)|./../_build/bin_plugs/|g' \
+        -e 's|\$(xrLibRootDir)|./../_build/lib/|g' \
+        -e 's|\$(xrBinDir)|./../_build/bin/$(Configuration)/|g' \
+        -e 's|\$(xrBinPlugDir)|./../_build/bin_plugs/$(Configuration)/|g' \
+        -e 's|\$(xrLibDir)|./../_build/lib/$(Configuration)/|g' \
+        -e 's|\$(xrIntDir)|./../_build/intermediate/$(Configuration)/|g' \
+        -e 's|\$(xrSdkDir)|./../sdk/|g' \
+        -e 's|\$(xrGameDir)|./../_build/_game/|g' \
+        -e 's|\$(xrGameBinDir)|./../_build/_game/bin/|g' \
+        -e 's|\$(xr3rdPartyDir)|./3rd party/|g' \
+        -e 's|\$(dxSdkDir)|./../sdk/include/dxsdk/|g' \
+        "$file"
 end
