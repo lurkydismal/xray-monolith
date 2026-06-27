3e9055bf8aa6afc84e6a61292810d474a24f7c98
LurkyDismal <lurkydismal@duck.com>
2026-06-27 12:46:01 +0000

    add progress bar to format script

--- format.fish
+++ format.fish
@@ -1,16 +1,8 @@
 #!/usr/bin/env fish
 
 fd --type f \
-    --extension c \
-    --extension cc \
-    --extension cpp \
-    --extension cxx \
-    --extension h \
-    --extension hh \
-    --extension hpp \
-    --extension hxx \
-    --extension inc \
-    --extension inl \
-    --extension m \
-    --extension mm \
-    --exec-batch clang-format -i
+    -e c -e cc -e cpp -e cxx \
+    -e h -e hh -e hpp -e hxx \
+    -e inc -e inl \
+    -e m -e mm |
+    parallel --bar clang-format -i {}
