b353fd0ad04c157108f14656b7c57da199dbb2db
LurkyDismal <lurkydismal@duck.com>
2026-06-28 16:17:14 +0000

    clang-cl cmane toolchain

---
+++ clang-cl-mingw-i686.cmake
@@ -0,0 +1,12 @@
+# clang-cl-mingw-i686.cmake
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86)
+
+set(CMAKE_C_COMPILER clang-cl)
+set(CMAKE_CXX_COMPILER clang-cl)
+
+set(CMAKE_C_COMPILER_TARGET i686-w64-windows-gnu)
+set(CMAKE_CXX_COMPILER_TARGET i686-w64-windows-gnu)
+
+set(CMAKE_LINKER lld-link)
