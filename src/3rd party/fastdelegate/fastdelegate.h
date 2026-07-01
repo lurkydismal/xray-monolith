f7f33509f9e3a97c3a3f326a4a1fec4ca1e31633
LurkyDismal <lurkydismal@duck.com>
2026-07-01 11:22:47 +0000

    Revert "update fast delegate"
    This reverts commit 4b355e089f23dc9a57219b4c97ae1d906d45e12c.

--- src/3rd party/fastdelegate/fastdelegate.h
+++ src/3rd party/fastdelegate/fastdelegate.h
@@ -1,4 +1,4 @@
-//                      FastDelegate.hpp
+﻿//                      FastDelegate.hpp
 //  Efficient delegates in C++ that generate only two lines of asm code!
 //  Documentation is found at http://www.codeproject.com/cpp/FastDelegate.asp
 //
@@ -76,7 +76,9 @@
 
 #ifndef FASTDELEGATE_HPP
 #define FASTDELEGATE_HPP
+#if _MSC_VER > 1000
 #pragma once
+#endif // _MSC_VER > 1000
 
 #include <cstring> // to allow <,> comparisons
 
