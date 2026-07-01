c0b8ceed5be4e8b5c39f3e1f9ee5f08cbc7fa7ef
LurkyDismal <lurkydismal@duck.com>
2026-07-01 10:12:42 +0000

    use MY_MSVC which is manually set to TRUE instead of MSVC check
    MSVC is FALSE for clang-cl

--- src/CMake/DefaultCXX.cmake
+++ src/CMake/DefaultCXX.cmake
@@ -2,7 +2,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/Default.cmake")
 
 set_config_specific_property("OUTPUT_DIRECTORY" "${CMAKE_SOURCE_DIR}$<$<NOT:$<STREQUAL:${CMAKE_VS_PLATFORM_NAME},Win32>>:/${CMAKE_VS_PLATFORM_NAME}>/${PROPS_CONFIG}")
 
-if(MSVC)
+if(MY_MSVC)
     create_property_reader("DEFAULT_CXX_EXCEPTION_HANDLING")
     create_property_reader("DEFAULT_CXX_DEBUG_INFORMATION_FORMAT")
 
