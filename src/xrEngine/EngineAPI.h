ab9066d1a77644f79fd3e314a14c458d482df787
LurkyDismal <lurkydismal@duck.com>
2026-07-02 08:39:28 +0000

    fix include in xrEngine EngineAPI h

--- src/xrEngine/EngineAPI.h
+++ src/xrEngine/EngineAPI.h
@@ -8,6 +8,8 @@
 #define AFX_ENGINEAPI_H__CF21372B_C8B8_4891_82FC_D872C84E1DD4__INCLUDED_
 #pragma once
 
+#include "../xrCore/clsid.h"
+
 // Abstract 'Pure' class for DLL interface
 class ENGINE_API DLL_Pure {
 public:
