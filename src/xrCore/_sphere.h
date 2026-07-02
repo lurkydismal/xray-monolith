de264332666b6aca0aad7dbcbebe4864476c1669
LurkyDismal <lurkydismal@duck.com>
2026-07-02 08:51:08 +0000

    fix _sphere.h

--- src/xrCore/_sphere.h
+++ src/xrCore/_sphere.h
@@ -88,7 +88,29 @@ public:
      */
     ICF ERP_Result intersect_full( const _vector3< T >& start,
                                    const _vector3< T >& dir,
-                                   T& dist ) const;
+                                   T& dist ) const {
+        using Fsphere = _sphere< float >;
+        using Dsphere = _sphere< double >;
+
+        int quantity;
+        float afT[ 2 ];
+        Fsphere::ERP_Result result =
+            intersect( start, dir, dist, quantity, afT );
+
+        if ( result == Fsphere::rpOriginInside ||
+             ( ( result == Fsphere::rpOriginOutside ) &&
+               ( afT[ 0 ] < dist ) ) ) {
+            switch ( result ) {
+                case Fsphere::rpOriginInside:
+                    dist = afT[ 0 ] < dist ? afT[ 0 ] : dist;
+                    break;
+                case Fsphere::rpOriginOutside:
+                    dist = afT[ 0 ];
+                    break;
+            }
+        }
+        return result;
+    }
 
     ICF ERP_Result intersect( const _vector3< T >& start,
                               const _vector3< T >& dir,
@@ -162,29 +184,6 @@ public:
 
 typedef _sphere< float > Fsphere;
 typedef _sphere< double > Dsphere;
-
-template < typename T >
-ICF _sphere< T >::ERP_Result _sphere< T >::intersect_full(
-    const _vector3< T >& start,
-    const _vector3< T >& dir,
-    T& dist ) const {
-    int quantity;
-    float afT[ 2 ];
-    Fsphere::ERP_Result result = intersect( start, dir, dist, quantity, afT );
-
-    if ( result == Fsphere::rpOriginInside ||
-         ( ( result == Fsphere::rpOriginOutside ) && ( afT[ 0 ] < dist ) ) ) {
-        switch ( result ) {
-            case Fsphere::rpOriginInside:
-                dist = afT[ 0 ] < dist ? afT[ 0 ] : dist;
-                break;
-            case Fsphere::rpOriginOutside:
-                dist = afT[ 0 ];
-                break;
-        }
-    }
-    return result;
-}
 
 template < class T >
 BOOL _valid( const _sphere< T >& s ) {
