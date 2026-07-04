a552f54955ba88d31f71566f1615a41731e23672
LurkyDismal <lurkydismal@duck.com>
2026-07-04 13:07:47 +0000

    chore(init): change

--- init.fish
+++ init.fish
@@ -3,3 +3,4 @@ fish_add_path -g /mnt/hdd/msvc_wine/bin/x86/
 set -x BIN /mnt/hdd/msvc_wine/bin/x86/
 source '/mnt/hdd/Projects/msvc-wine/msvcenv-native.fish'
 cmake-gui -S src -B src/build -G "Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+CC=cl CXX=cl cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
