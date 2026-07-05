595ddde646b7ed2963e7196b9631b0776be63465
LurkyDismal <lurkydismal@duck.com>
2026-07-05 06:03:36 +0000

    chore(init): update fish script

--- init.fish
+++ init.fish
@@ -1,6 +1,5 @@
 #!/usr/bin/env fish
-fish_add_path -g /mnt/hdd/msvc_wine/bin/x86/
-set -x BIN /mnt/hdd/msvc_wine/bin/x86/
+fish_add_path -g /mnt/hdd/msvc_wine/bin/x64/
+set -x BIN /mnt/hdd/msvc_wine/bin/x64/
 source '/mnt/hdd/Projects/msvc-wine/msvcenv-native.fish'
-cmake-gui -S src -B src/build -G "Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-CC=cl CXX=cl cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# CC=cl CXX=cl cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
