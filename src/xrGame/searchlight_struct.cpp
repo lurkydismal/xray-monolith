166a819446add3e768fb0af91cf45d7dc485ed41
LurkyDismal <lurkydismal@duck.com>
2026-07-06 09:01:47 +0000

    fix(xrGame): searchlight_struct cpp

--- src/xrGame/searchlight_struct.cpp
+++ src/xrGame/searchlight_struct.cpp
@@ -311,11 +311,12 @@ void SProjectorControl::SetParam(int id, Fvector val)
 {
 	switch (id)
 	{
-	case CProjector::eDesiredPos:
+	case CProjector::eDesiredPos: {
 		Fvector vec = Fmatrix().mul_43(O->XFORM(), O->Visual()->dcast_PKinematics()->LL_GetTransform(m_rotate_y_bone)).c;
 		m_desire_dir.sub(val, vec).normalize_safe();
 		m_enable_ang = false;
 		break;
+	}
 	case CProjector::eDesiredDir:
 		m_desire_dir.set(val).normalize_safe();
 		m_enable_ang = false;
