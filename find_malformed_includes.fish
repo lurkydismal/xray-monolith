702ea91c1d24748214fdb67a67359b74363c4086
LurkyDismal <lurkydismal@duck.com>
2026-07-07 12:38:40 +0000

    feat(script): find malformed includes fish now exludes sdk/ and 'src/3rd party/' directories

--- find_malformed_includes.fish
+++ find_malformed_includes.fish
@@ -1,24 +1,32 @@
 #!/usr/bin/env fish
 
-rg -n '#\s*include\s*[<"]([^">]+)[">]' . \
-    | while read -l match
-    set file (string split : $match)[1]
-    set line (string split : $match)[2]
-    set inc (string replace -r '.*[<"]([^">]+)[">].*' '$1' $match)
+rg -n \
+    -g '!sdk/**' \
+    -g '!src/3rd party/**' \
+    '#\s*include\s*[<"]([^">]+)[">]' . |
+    while read -l match
+        set file (string split : $match)[1]
+        set line (string split : $match)[2]
+        set inc (string replace -r '.*[<"]([^">]+)[">].*' '$1' $match)
 
-    set dir (path dirname $file)
-    set candidate $dir/$inc
+        set dir (path dirname $file)
+        set candidate $dir/$inc
 
-    if test -e $candidate
-        continue
+        if test -e $candidate
+            continue
+        end
+
+        set found (fd \
+        -E sdk \
+        -E 'src/3rd party' \
+        -i --full-path \
+        "^.*/"(string escape --style=regex $inc)'$' \
+        $dir)
+
+        if test -n "$found"
+            echo "$file:$line"
+            echo "  include: $inc"
+            echo "  actual : $found"
+            echo
+        end
     end
-
-    set found (fd -i --full-path "^.*/"(string escape --style=regex $inc)'$' $dir)
-
-    if test -n "$found"
-        echo "$file:$line"
-        echo "  include: $inc"
-        echo "  actual : $found"
-        echo
-    end
-end
