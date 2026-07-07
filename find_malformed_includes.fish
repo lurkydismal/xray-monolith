dfdddfff1b46e6f3e343ba9fb3495dc09dc4a864
LurkyDismal <lurkydismal@duck.com>
2026-07-07 06:53:53 +0000

    feat(script): add find malformed includes fish

---
+++ find_malformed_includes.fish
@@ -0,0 +1,24 @@
+#!/usr/bin/env fish
+
+rg -n '#\s*include\s*[<"]([^">]+)[">]' . \
+    | while read -l match
+    set file (string split : $match)[1]
+    set line (string split : $match)[2]
+    set inc (string replace -r '.*[<"]([^">]+)[">].*' '$1' $match)
+
+    set dir (path dirname $file)
+    set candidate $dir/$inc
+
+    if test -e $candidate
+        continue
+    end
+
+    set found (fd -i --full-path "^.*/"(string escape --style=regex $inc)'$' $dir)
+
+    if test -n "$found"
+        echo "$file:$line"
+        echo "  include: $inc"
+        echo "  actual : $found"
+        echo
+    end
+end
