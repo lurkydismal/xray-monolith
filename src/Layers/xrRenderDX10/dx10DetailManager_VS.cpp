346868853827ce58ee019764a8066bd251c33843
LurkyDismal <lurkydismal@duck.com>
2026-07-07 14:42:56 +0000

    fix(Layers/xrRenderDX10): caching

--- src/Layers/xrRenderDX10/dx10DetailManager_VS.cpp
+++ src/Layers/xrRenderDX10/dx10DetailManager_VS.cpp
@@ -1,8 +1,8 @@
 #include "stdafx.h"
 #include "../xrRender/DetailManager.h"
 
-#include "../../xrEngine/igame_persistent.h"
-#include "../../xrEngine/environment.h"
+#include "../../xrEngine/IGame_Persistent.h"
+#include "../../xrEngine/Environment.h"
 
 #include "../xrRenderDX10/dx10BufferUtils.h"
 
