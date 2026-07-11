56f7507344a2c4ec10a0fea7ac572ebc41852b0f
LurkyDismal <lurkydismal@duck.com>
2026-07-11 09:18:26 +0000

    Revert "build(cmake): static link exe and shared link tests"
    This reverts commit 4c18093ba71f7cf81184a4ec06c508174959ee56.

--- src/CMake/XRayTesting.cmake
+++ src/CMake/XRayTesting.cmake
@@ -63,8 +63,8 @@ function(add_xray_discovered_tests)
     target_link_libraries(xray_unit_tests PRIVATE
         GTest::gtest_main
         GTest::gmock
-        xrCore_shared
-        xrEngine_shared
+        xrCore
+        xrEngine
     )
 
     # Match the engine's C++ language level for test translation units.
