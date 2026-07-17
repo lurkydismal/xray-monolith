d2d58a4ca3ce931f23aad26a0cb376111ab1f78e
lurkydismal <lurkydismal@duck.com>
2026-07-17 06:31:40 +0000

    fix(xrServerEntities): associative_vector_inline h

--- src/xrServerEntities/associative_vector_inline.h
+++ src/xrServerEntities/associative_vector_inline.h
@@ -233,7 +233,7 @@ IC typename _associative_vector::insert_result _associative_vector::insert(const
 	actualize();
 	bool found = true;
 	iterator I = lower_bound(value.first);
-	if (I == end() || operator()(value.first, (*I).first))
+	if (I == end() || this->operator()(value.first, (*I).first))
 	{
 		I = inherited::insert(I, value);
 		found = false;
@@ -248,10 +248,10 @@ IC typename _associative_vector::iterator _associative_vector::insert(iterator w
 {
 	if (
 		(where != end()) &&
-		(operator()(*where, value)) &&
+		(this->operator()(*where, value)) &&
 		((where - begin()) == size()) &&
-		(!operator()(value, *(where + 1))) &&
-		(operator()(*(where + 1), value))
+		(!this->operator()(value, *(where + 1))) &&
+		(this->operator()(*(where + 1), value))
 	)
 		return (inherited::insert(where, value));
 
@@ -282,7 +282,7 @@ IC typename _associative_vector::iterator _associative_vector::find(const key_ty
 	if (I == end())
 		return (end());
 
-	if (operator()(key, (*I).first))
+	if (this->operator()(key, (*I).first))
 		return (end());
 
 	return (I);
@@ -296,7 +296,7 @@ IC typename _associative_vector::const_iterator _associative_vector::find(const 
 	if (I == end())
 		return (end());
 
-	if (operator()(key, (*I).first))
+	if (this->operator()(key, (*I).first))
 		return (end());
 
 	return (I);
@@ -317,10 +317,10 @@ IC typename _associative_vector::equal_range_result _associative_vector::equal_r
 	if (I == end())
 		return (equal_range_result(end(), end()));
 
-	if (operator()(key, (*I).first))
+	if (this->operator()(key, (*I).first))
 		return (equal_range_result(I, I));
 
-	VERIFY(!operator()(key,(*I).first));
+	VERIFY(!this->operator()(key,(*I).first));
 	return (equal_range_result(I, I + 1));
 }
 
@@ -332,10 +332,10 @@ IC typename _associative_vector::const_equal_range_result _associative_vector::e
 	if (I == end())
 		return (const_equal_range_result(end(), end()));
 
-	if (operator()(key, (*I).first))
+	if (this->operator()(key, (*I).first))
 		return (const_equal_range_result(I, I));
 
-	VERIFY(!operator()(key,(*I).first));
+	VERIFY(!this->operator()(key,(*I).first));
 	return (const_equal_range_result(I, I + 1));
 }
 
