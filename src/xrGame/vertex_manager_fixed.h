daf7f2b9eb286e33b9ce59cab00bea43d24610a5
lurkydismal <lurkydismal@duck.com>
2026-07-17 09:04:13 +0000

    fix(xrGame): vertex_manager_fixed h

--- src/xrGame/vertex_manager_fixed.h
+++ src/xrGame/vertex_manager_fixed.h
@@ -42,11 +42,11 @@ struct CVertexManagerFixed
 		template <typename _T1, typename _T2> class _index_vertex = CEmptyClassTemplate2,
 		typename _data_storage = CBuilderAllocatorConstructor
 	>
-	class CDataStorage : public _data_storage::template CDataStorage<VertexManager<_vertex>::_vertex>
+	class CDataStorage : public _data_storage::template CDataStorage<VertexManager<_vertex>::template _vertex>
 	{
 	public:
 		typedef typename _data_storage::template CDataStorage<
-			VertexManager<_vertex>::_vertex
+			VertexManager<_vertex>::template _vertex
 		> inherited;
 		typedef typename inherited::CGraphVertex CGraphVertex;
 		typedef typename CGraphVertex::_index_type _index_type;
