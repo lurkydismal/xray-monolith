768b3b32e2dd63df8f9ad826397ec99d22140702
lurkydismal <lurkydismal@duck.com>
2026-07-17 09:18:31 +0000

    fix(xrGame): path_manager_solver h

--- src/xrGame/path_manager_solver.h
+++ src/xrGame/path_manager_solver.h
@@ -47,7 +47,6 @@ protected:
 
 public:
 	using const_iterator = typename _Graph::const_iterator;
-	using _solver_dist_type = typename _Graph::_solver_dist_type;
 
 protected:
 	xr_vector<_edge_type>* m_edge_path;
