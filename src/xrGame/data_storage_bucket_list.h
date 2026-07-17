d8c5705fe40c0cb002c7dbd63775d79ec7db332d
lurkydismal <lurkydismal@duck.com>
2026-07-17 08:22:36 +0000

    fix(xrGame): data_storage_bucker_list and inline h

--- src/xrGame/data_storage_bucket_list.h
+++ src/xrGame/data_storage_bucket_list.h
@@ -34,17 +34,19 @@ struct CDataStorageBucketList
 		template <typename _T> class _vertex = CEmptyClassTemplate
 	>
 	class CDataStorage : public CDataStorageDoubleLinkedList<false>::CDataStorage<
-			_data_storage, BucketList<_vertex>::_vertex>
+			_data_storage, BucketList<_vertex>::template _vertex>
 	{
 	public:
 		typedef typename CDataStorageDoubleLinkedList<false>::CDataStorage<
 			_data_storage,
-			BucketList<_vertex>::_vertex
+			BucketList<_vertex>::template _vertex
 		> inherited;
 		typedef typename inherited::inherited_base inherited_base;
 		typedef typename inherited::CGraphVertex CGraphVertex;
 		typedef typename CGraphVertex::_dist_type _dist_type;
 		typedef typename CGraphVertex::_index_type _index_type;
+
+        using inherited::current_path_id;
 
 	protected:
 		_dist_type m_min_bucket_value;
