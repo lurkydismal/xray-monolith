e3b6feff7b8f9082fd6b43b5a4d60d4866a67a86
lurkydismal <lurkydismal@duck.com>
2026-07-17 09:04:22 +0000

    fix(xrGame): vertex_manager_hash_fixed h

--- src/xrGame/vertex_manager_hash_fixed.h
+++ src/xrGame/vertex_manager_hash_fixed.h
@@ -53,7 +53,7 @@ struct CVertexManagerHashFixed
 		template <typename _T1, typename _T2> class _index_vertex = CEmptyClassTemplate2,
 		typename _data_storage = CBuilderAllocatorConstructor
 	>
-	class CDataStorage : public _data_storage::template CDataStorage<VertexManager<_vertex>::_vertex>
+	class CDataStorage : public _data_storage::template CDataStorage<VertexManager<_vertex>::template _vertex>
 	{
 	public:
 		typedef typename _data_storage::template CDataStorage<
