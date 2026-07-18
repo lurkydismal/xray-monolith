fb6d9bcac3d9b10ebd67c30cd2424e943015c784
LurkyDismal <lurkydismal@duck.com>
2026-07-18 07:24:06 +0000

    fix(xrGame): CustomDevice cpp

--- src/xrGame/CustomDevice.cpp
+++ src/xrGame/CustomDevice.cpp
@@ -1,4 +1,4 @@
-﻿#include "StdAfx.h"
+#include "StdAfx.h"
 #include "CustomDevice.h"
 #include "Inventory.h"
 #include "Actor.h"
@@ -62,7 +62,7 @@ bool CCustomDevice::CheckCompatibilityInt(CHudItem* itm, u16* slot_to_activate)
 			bres = bres &&
 			(W->GetState() != CHUDState::eBore) &&
 			(W->GetState() != CWeapon::eReload) &&
-			(W->GetState() != CWeapon::eSwitch) && 
+			(W->GetState() != CWeapon::eSwitch) &&
 			(m_bCanBeZoomed || !W->IsZoomed());
 	}
 	return bres;
@@ -151,7 +151,7 @@ void CCustomDevice::OnStateSwitch(u32 S, u32 oldState)
 			m_fZoomfactor = .51f;
 
 		m_bZoomed && HudAnimationExist("anm_zoom_show")
-			? PlayHUDMotion("anm_zoom_show", FALSE, this, GetState(), 1.f, 0.f, false) 
+			? PlayHUDMotion("anm_zoom_show", FALSE, this, GetState(), 1.f, 0.f, false)
 			: PlayHUDMotion(m_bFastAnimMode ? "anm_show_fast" : "anm_show", FALSE, this, GetState(), 1.f, 0.f, false);
 		SetPending(TRUE);
 	}
@@ -163,7 +163,7 @@ void CCustomDevice::OnStateSwitch(u32 S, u32 oldState)
 			m_sounds.PlaySound("sndHide", Fvector().set(0, 0, 0), this, true, false);
 
 			m_fZoomfactor > .5f && (oldState == eIdleZoom || oldState == eIdleZoomIn || oldState == eIdleZoomOut || oldState == eShowing) && HudAnimationExist(m_bFastAnimMode ? "anm_zoom_hide_fast" : "anm_zoom_hide")
-				? PlayHUDMotion(m_bFastAnimMode ? "anm_zoom_hide_fast" : "anm_zoom_hide", TRUE, this, GetState()) 
+				? PlayHUDMotion(m_bFastAnimMode ? "anm_zoom_hide_fast" : "anm_zoom_hide", TRUE, this, GetState())
 				: PlayHUDMotion(m_bFastAnimMode ? "anm_hide_fast" : "anm_hide", TRUE, this, GetState());
 			SetPending(TRUE);
 
@@ -189,18 +189,22 @@ void CCustomDevice::OnStateSwitch(u32 S, u32 oldState)
 	{
 		m_bZoomed = true;
 
-		HudAnimationExist(m_bOldZoom ? "anm_zoom_in" : "anm_zoom")
-			? PlayHUDMotion(m_bOldZoom ? "anm_zoom_in" : "anm_zoom", TRUE, this, GetState())
-			: SwitchState(eIdleZoom);
+		if (HudAnimationExist(m_bOldZoom ? "anm_zoom_in" : "anm_zoom") ) {
+            PlayHUDMotion(m_bOldZoom ? "anm_zoom_in" : "anm_zoom", TRUE, this, GetState());
+        } else {
+            SwitchState(eIdleZoom);
+        }
 		SetPending(FALSE);
 	}
 	break;
 	case eIdleZoomOut:
 	{
 		m_bZoomed = false;
-		HudAnimationExist(m_bOldZoom ? "anm_zoom_out" : "anm_zoom")
-			? PlayHUDMotion(m_bOldZoom ? "anm_zoom_out" : "anm_zoom", TRUE, this, GetState())
-			: SwitchState(eIdle);
+		if (HudAnimationExist(m_bOldZoom ? "anm_zoom_out" : "anm_zoom") ) {
+			PlayHUDMotion(m_bOldZoom ? "anm_zoom_out" : "anm_zoom", TRUE, this, GetState());
+        } else {
+            SwitchState(eIdle);
+        }
 		SetPending(FALSE);
 	}
 	break;
@@ -211,7 +215,7 @@ void CCustomDevice::OnStateSwitch(u32 S, u32 oldState)
 			PlayHUDMotion("anm_throw_start", TRUE, this, GetState());
 			SetPending(TRUE);
 		}
-		else 
+		else
 			SwitchState(eIdleThrow);
 	}
 	break;
@@ -390,7 +394,7 @@ void CCustomDevice::UpdateVisibility()
 						SwitchState(eIdleZoomOut);
 					}
 					else if (m_bNeedActivation && !bClimb && CheckCompatibilityInt(i0->m_parent_hud_item, 0))
-					{ 
+					{
 						ShowDevice(true);
 					}
 				}
@@ -442,7 +446,7 @@ void CCustomDevice::UpdateWork()
 void CCustomDevice::TurnDeviceInternal(bool b)
 {
 	m_bWorking = b;
-	
+
 	if (ParentIsActor())
 	{
 		if (m_bWorking)
