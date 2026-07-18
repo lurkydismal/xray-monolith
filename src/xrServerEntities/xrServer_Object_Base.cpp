0faef35394a63bbfd48c790d17bb06d0a197cd50
LurkyDismal <lurkydismal@duck.com>
2026-07-18 08:29:08 +0000

    fix(xrServerEntities): xrServer_Object_Base cpp

--- src/xrServerEntities/xrServer_Object_Base.cpp
+++ src/xrServerEntities/xrServer_Object_Base.cpp
@@ -6,6 +6,7 @@
 //	Description : Server base object
 ////////////////////////////////////////////////////////////////////////////
 
+#include "FS.h"
 #include "stdafx.h"
 #include "xrServer_Objects.h"
 #include "xrMessages.h"
@@ -181,16 +182,19 @@ CSE_Motion* CSE_Abstract::motion()
 
 CInifile& CSE_Abstract::spawn_ini()
 {
-	if (!m_ini_file)
+	if (!m_ini_file) {
 #pragma warning(push)
 #pragma warning(disable:4238)
+        IReader reader(
+            (void*)(*(m_ini_string)),
+            m_ini_string.size()
+        );
+
 		m_ini_file = xr_new<CInifile>(
-			&IReader(
-				(void*)(*(m_ini_string)),
-				m_ini_string.size()
-			),
+			&reader,
 			FS.get_path("$game_config$")->m_Path
 		);
+    }
 #pragma warning(pop)
 	return (*m_ini_file);
 }
