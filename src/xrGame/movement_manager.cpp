e9eda23ce986652c83915ecf31977e860664afa6
LurkyDismal <lurkydismal@duck.com>
2026-07-19 12:35:04 +0000

    fix(xrGame): movement_manager cpp

--- src/xrGame/movement_manager.cpp
+++ src/xrGame/movement_manager.cpp
@@ -15,7 +15,7 @@
 #include "level_path_manager.h"
 #include "detail_path_manager.h"
 #include "patrol_path_manager.h"
-#include "xrmessages.h"
+#include "xrMessages.h"
 #include "ai_object_location.h"
 #include "CustomMonster.h"
 #include "location_manager.h"
@@ -25,7 +25,7 @@
 #include "mt_config.h"
 //#include "CustomMonster.h"
 
-// Lain: added 
+// Lain: added
 #include "steering_behaviour.h"
 
 using namespace MovementManager;
