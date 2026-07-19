07a4d6e6a96e95f4ff2406fef446a177e2bf48ee
LurkyDismal <lurkydismal@duck.com>
2026-07-19 08:24:55 +0000

    fix(xrGame): condition_stage_inline h

--- src/xrGame/condition_state_inline.h
+++ src/xrGame/condition_state_inline.h
@@ -57,7 +57,7 @@ IC void CConditionStateAbstract::remove_condition(const typename COperatorCondit
 {
 	typename xr_vector<COperatorCondition>::iterator I = std::lower_bound(m_conditions.begin(), m_conditions.end(),
 	                                                             COperatorCondition(
-		                                                             condition, COperatorCondition::_value_type(0)));
+		                                                             condition, typename COperatorCondition::_value_type(0)));
 	THROW((I != m_conditions.end()) && ((*I).condition() == condition));
 	if ((I != m_conditions.end()) && ((*I).condition() == condition))
 	{
