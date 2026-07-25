a8d81d24e5b4555f0ba2ec10429835bde5ba87a4
LurkyDismal <lurkydismal@duck.com>
2026-07-25 08:02:22 +0000

    todo(rust): bindgen

--- src/rust_lib/src/lib.rs
+++ src/rust_lib/src/lib.rs
@@ -1,3 +1,9 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
---
+++ src/rust_lib/wrapper.h
@@ -0,0 +1,1 @@
+#include <D3DX11.h>
