d43465c1a843f7a165190a61fc6706e15f25e0a5
LurkyDismal <lurkydismal@duck.com>
2026-08-07 02:10:05 +0000

    build(cmake): test coverage

--- src/CMake/CompilerOptions.cmake
+++ src/CMake/CompilerOptions.cmake
@@ -66,3 +66,15 @@ if(IS_CLANG AND MSVC)
         -clang:-march=native
     )
 endif()
+
+if(XRay_BUILD_COVERAGE)
+    add_compile_options(
+        -O0
+        -g
+        --coverage
+    )
+
+    add_link_options(
+        --coverage
+    )
+endif()
