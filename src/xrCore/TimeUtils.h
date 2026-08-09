f1961f71f81b279b6403c54aa5e009aab42f7c9c
LurkyDismal <lurkydismal@duck.com>
2026-08-09 01:47:50 +0000

    feat(TimeUtils): split into cpp

--- src/xrCore/TimeUtils.h
+++ src/xrCore/TimeUtils.h
@@ -18,36 +18,7 @@ xr_time_t xr_chrono_to_time_t(TP tp)
 	return system_clock::to_time_t(sctp);
 }
 
-xr_string getCurrentTimeStamp(const char* format = "%H:%M:%S")
-{
-	using namespace std::chrono;
+xr_string getCurrentTimeStamp(const char* format = "%H:%M:%S");
 
-	// get current time
-	auto now = system_clock::now();
-
-	// get number of milliseconds for the current second
-	// (remainder after division into seconds)
-	auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
-
-	// convert to std::time_t in order to convert to std::tm (broken time)
-	xr_time_t timer = system_clock::to_time_t(now);
-
-	// convert to broken time
-	std::tm bt = *std::localtime(&timer);
-
-	std::ostringstream oss;
-
-	oss << std::put_time(&bt, format); // HH:MM:SS
-	oss << '.' << std::setfill('0') << std::setw(3) << ms.count();
-
-	return oss.str().c_str();
-}
-
-xr_string timeInDMYHMSMMM()
-{
-	return getCurrentTimeStamp("%d.%m.%Y %H:%M:%S");
-}
-xr_string timeInHMSMMM()
-{
-	return getCurrentTimeStamp("%H:%M:%S");
-}
+xr_string timeInDMYHMSMMM();
+xr_string timeInHMSMMM();
