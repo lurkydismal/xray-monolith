8140d448b5640a482ada0b9927e7082f4e6f4dcf
LurkyDismal <lurkydismal@duck.com>
2026-08-09 05:28:32 +0000

    test(_noncopyable): add

---
+++ src/xrCore/_noncopyable.test.cpp
@@ -0,0 +1,66 @@
+#include "stdafx.h"
+
+#include "_noncopyable.h"
+
+#include <gtest/gtest.h>
+
+namespace
+{
+    class TestNoncopyable : public xray::noncopyable
+    {
+    public:
+        TestNoncopyable() = default;
+        ~TestNoncopyable() override = default;
+
+        int value = 42;
+    };
+}
+
+TEST(Noncopyable, IsDefaultConstructible)
+{
+    static_assert(std::is_default_constructible_v<xray::noncopyable>);
+    static_assert(std::is_default_constructible_v<TestNoncopyable>);
+
+    SUCCEED();
+}
+
+TEST(Noncopyable, IsDestructible)
+{
+    static_assert(std::is_destructible_v<xray::noncopyable>);
+    static_assert(std::is_destructible_v<TestNoncopyable>);
+
+    SUCCEED();
+}
+
+TEST(Noncopyable, IsNotCopyConstructible)
+{
+    static_assert(!std::is_copy_constructible_v<xray::noncopyable>);
+    static_assert(!std::is_copy_constructible_v<TestNoncopyable>);
+
+    SUCCEED();
+}
+
+TEST(Noncopyable, IsNotCopyAssignable)
+{
+    static_assert(!std::is_copy_assignable_v<xray::noncopyable>);
+    static_assert(!std::is_copy_assignable_v<TestNoncopyable>);
+
+    SUCCEED();
+}
+
+TEST(Noncopyable, CanBeUsedPolymorphically)
+{
+    TestNoncopyable object;
+
+    xray::noncopyable* base = &object;
+
+    ASSERT_NE(base, nullptr);
+}
+
+TEST(Noncopyable, HasVirtualDestructor)
+{
+    static_assert(std::has_virtual_destructor_v<xray::noncopyable>);
+    static_assert(std::has_virtual_destructor_v<TestNoncopyable>);
+
+    SUCCEED();
+}
