09c941be4aa45a1d1d4523179d3c1d2075b59aa7
LurkyDismal <lurkydismal@duck.com>
2026-08-11 10:56:37 +0000

    fix(xrParticles): particle_manager OnFrame() remove override
    Does override nothing

--- src/xrParticles/particle_manager.h
+++ src/xrParticles/particle_manager.h
@@ -56,6 +56,6 @@ namespace PAPI
         virtual ParticleAction* CreateAction(PActionEnum action_id);
         virtual u32					LoadActions(int alist_id, IReader& R);
         virtual void				SaveActions(int alist_id, IWriter& W);
-        virtual void                OnFrame() override;
+        virtual void                OnFrame();
     };
 };
