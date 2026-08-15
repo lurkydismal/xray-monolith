8a6ebd1d8b3af49fde2b43a6b35da2e73b8899fa
LurkyDismal <lurkydismal@duck.com>
2026-08-15 08:47:06 +0000

    feat(format): ignore sdk and 3rd_party

--- format.fish
+++ format.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 
-fd --type f \
+fd \
+    --exclude sdk --exclude src/3rd_party \
+    --type f \
     -e c -e cc -e cpp -e cxx \
     -e h -e hh -e hpp -e hxx \
     -e inc -e inl \
