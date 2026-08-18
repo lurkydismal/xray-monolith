6a2283787c1f799ec3297d303534602f8478e1fe
LurkyDismal <lurkydismal@duck.com>
2026-08-18 09:43:18 +0000

    build(CompilerOptions): refactor

--- src/CMake/CompilerOptions.cmake
+++ src/CMake/CompilerOptions.cmake
@@ -1,60 +1,25 @@
 ################################################################################
 # Global compiler options
 ################################################################################
-# TODO: Refactor
-if(MSVC)
-    # remove default flags provided with CMake for MSVC
-    set(CMAKE_C_FLAGS "")
-    set(CMAKE_C_FLAGS_DX10 "")
-    set(CMAKE_C_FLAGS_DX10AVX "")
-    set(CMAKE_C_FLAGS_DX11 "")
-    set(CMAKE_C_FLAGS_DX11AVX "")
-    set(CMAKE_C_FLAGS_DX8 "")
-    set(CMAKE_C_FLAGS_DX8AVX "")
-    set(CMAKE_C_FLAGS_DX9 "")
-    set(CMAKE_C_FLAGS_DX9AVX "")
-    set(CMAKE_C_FLAGS_PROFILEDDX11 "")
-    set(CMAKE_C_FLAGS_RELEASE "")
-    set(CMAKE_C_FLAGS_RELEASEAVX "")
-    set(CMAKE_C_FLAGS_VERIFIED "")
-    set(CMAKE_C_FLAGS_VERIFIEDDX11 "")
-    set(CMAKE_C_FLAGS_VERIFIEDDX11ASAN "")
-    set(CMAKE_C_FLAGS_VERIFIEDDX8 "")
-    set(CMAKE_C_FLAGS_VERIFIEDDX9 "")
-    set(CMAKE_CXX_FLAGS "")
-    set(CMAKE_CXX_FLAGS_DX10 "")
-    set(CMAKE_CXX_FLAGS_DX10AVX "")
-    set(CMAKE_CXX_FLAGS_DX11 "")
-    set(CMAKE_CXX_FLAGS_DX11AVX "")
-    set(CMAKE_CXX_FLAGS_DX8 "")
-    set(CMAKE_CXX_FLAGS_DX8AVX "")
-    set(CMAKE_CXX_FLAGS_DX9 "")
-    set(CMAKE_CXX_FLAGS_DX9AVX "")
-    set(CMAKE_CXX_FLAGS_PROFILEDDX11 "")
-    set(CMAKE_CXX_FLAGS_RELEASE "")
-    set(CMAKE_CXX_FLAGS_RELEASEAVX "")
-    set(CMAKE_CXX_FLAGS_VERIFIED "")
-    set(CMAKE_CXX_FLAGS_VERIFIEDDX11 "")
-    set(CMAKE_CXX_FLAGS_VERIFIEDDX11ASAN "")
-    set(CMAKE_CXX_FLAGS_VERIFIEDDX8 "")
-    set(CMAKE_CXX_FLAGS_VERIFIEDDX9 "")
-endif()
+foreach(cfg IN LISTS CMAKE_CONFIGURATION_TYPES)
+    string(TOUPPER "${cfg}" CFG)
 
-# NOTE: With this
-# foreach(cfg IN LISTS CMAKE_CONFIGURATION_TYPES)
-#     string(TOUPPER "${cfg}" CFG)
-#
-#     set(CMAKE_C_FLAGS_${CFG} "" CACHE STRING "" FORCE)
-#     set(CMAKE_CXX_FLAGS_${CFG} "" CACHE STRING "" FORCE)
-# endforeach()
+    set(CMAKE_C_FLAGS_${CFG} "")
+    set(CMAKE_CXX_FLAGS_${CFG} "")
+endforeach()
 
-# TODO: Add clang-only options
-if(IS_CLANG)
+if(IS_CLANG AND NOT MSVC)
     add_compile_options(
+        -Wno-c++11-narrowing
+        -Wno-microsoft-cast
+        -Wno-microsoft-enum-value
+        -Wno-invalid-offsetof
+        -fms-extensions
+        # -fms-compatibility
+        -fdelayed-template-parsing
+        -march=native
     )
-endif()
-
-if(IS_CLANG AND MSVC)
+elseif(IS_CLANG AND MSVC)
     add_compile_options(
         -clang:-Wno-c++11-narrowing
         -clang:-Wno-microsoft-cast
