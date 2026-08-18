eea3749281c98da7d467239f08c3f56e414e431e
LurkyDismal <lurkydismal@duck.com>
2026-08-18 09:41:08 +0000

    chore(toolchain): add clang-mingw-x64 for windows

---
+++ clang-mingw-x64.cmake
@@ -0,0 +1,14 @@
+# clang-mingw-x64.cmake
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x64)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang)
+
+set(CMAKE_C_COMPILER_TARGET x86_64-w64-windows-gnu)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-w64-windows-gnu)
+
+set(CMAKE_LINKER ld.lld)
+
+set(CMAKE_AR llvm-ar)
