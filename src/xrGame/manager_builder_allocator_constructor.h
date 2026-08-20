fed5b29e6b72496504d7f2d136c113d69ea188fd
LurkyDismal <lurkydismal@duck.com>
2026-08-20 17:11:37 +0000

    fix(xrGame): manager_builder_allocator_constructor and inline h

--- src/xrGame/manager_builder_allocator_constructor.h
+++ src/xrGame/manager_builder_allocator_constructor.h
@@ -15,8 +15,8 @@ template <
 	typename _builder,
 	typename _allocator,
 	template <
-		typename _builder,
-		typename _allocator
+		typename __builder,
+		typename __allocator
 	>
 	class _builder_allocator_constructor = CBuilderAllocatorConstructor
 >
