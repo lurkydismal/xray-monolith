ed5655a8385e2de8a3e0ae78346860f697dfb28a
lurkydismal <lurkydismal@duck.com>
2026-08-20 05:04:49 +0000

    build(cmake): fix some clang and clang-cl checks

--- src/CMake/LinkerOptions.cmake
+++ src/CMake/LinkerOptions.cmake
@@ -89,7 +89,7 @@ endif()
 ################################################################################
 set(INCREMENTAL_LINKING_ENABLED OFF)
 if(ENABLE_INCREMENTAL_LINKING)
-    if(CLANG_CL_MINGW OR CLANG_CL_MSVC)
+    if(IS_CLANG_CL_MINGW OR IS_CLANG_CL_MSVC)
         if(NOT ENABLE_IPO)
             set(INCREMENTAL_LINKING_ENABLED ON)
             message(WARNING "Incremental linking enabled")
