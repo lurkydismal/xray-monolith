bc0ab4a41d1df9695dfcd4cfe5d7ffcd00c0850c
lurkydismal <lurkydismal@duck.com>
2026-08-21 10:48:40 +0000

    fix(xrGame): split door_state

---
+++ src/xrGame/door_state.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace doors {
+
+enum door_state
+{
+    door_state_open,
+    door_state_closed,
+};
+
+}
--- src/xrGame/doors.h
+++ src/xrGame/doors.h
@@ -7,16 +7,20 @@
 #ifndef DOORS_H_INCLUDED
 #define DOORS_H_INCLUDED
 
+#include "door_state.hpp"
+
 namespace doors
 {
 	class door;
 	typedef xr_vector<door*> doors_type;
 
+#if 0
 	enum door_state
 	{
 		door_state_open,
 		door_state_closed,
 	}; // enum door_state
+#endif
 
 	extern float const g_door_length;
 	extern float const g_door_open_time;
