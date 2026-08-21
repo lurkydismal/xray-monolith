35ffdfb3245f58c8b2b40c4861014415e470d8d3
lurkydismal <lurkydismal@duck.com>
2026-08-21 10:48:47 +0000

    fix(xrGame): alife_registry_wrapper h

--- src/xrGame/alife_registry_wrapper.h
+++ src/xrGame/alife_registry_wrapper.h
@@ -43,7 +43,7 @@ const typename _registry_type::_data* CALifeRegistryWrapper<_registry_type>::obj
 		if (I == local_registry.end())
 		{
 			typename _registry_type::_data new_registry;
-			std::pair<_registry_type::iterator, bool> p = local_registry.insert(std::make_pair(id, new_registry));
+			std::pair<typename _registry_type::iterator, bool> p = local_registry.insert(std::make_pair(id, new_registry));
 			VERIFY(p.second);
 			return &(*local_registry.find(id)).second;
 		}
@@ -66,7 +66,7 @@ typename _registry_type::_data& CALifeRegistryWrapper<_registry_type>::objects(u
 		if (I == local_registry.end())
 		{
 			typename _registry_type::_data new_registry;
-			std::pair<_registry_type::iterator, bool> p = local_registry.insert(std::make_pair(id, new_registry));
+			std::pair<typename _registry_type::iterator, bool> p = local_registry.insert(std::make_pair(id, new_registry));
 			VERIFY(p.second);
 			return (*local_registry.find(id)).second;
 		}
